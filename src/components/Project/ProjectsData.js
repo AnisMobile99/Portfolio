e6feaf3cada3c593cb66c567f841e1c9900095b8
AnisMobile99 <anis.salahbey.dz@gmail.com>
2024-12-29 13:09:38 +0100

    [fixed]projectsData refresh

--- src/components/Project/ProjectsData.js
+++ src/components/Project/ProjectsData.js
@@ -1,83 +1,93 @@
 export const projectsData = [
   {
     id: 1,
-    title: "Application Web pour coachs/sportifs",
+    title: "Application Web pour coachs et sportifs",
     description:
-      "Plateforme permettant aux coachs et sportifs de créer des programmes personnalisés et suivre leurs performances.",
+      "Une application complète permettant aux coachs et sportifs de concevoir des programmes personnalisés, suivre les performances et gérer les interactions via une interface intuitive.",
     image: "https://www.koacher.app/phoneheader.png",
-    tags: ["React.js", "TailwindCSS", "TypeScript", "MongoDB"],
+    tags: ["React.js", "TailwindCSS", "TypeScript", "MongoDB", "React Native"],
     link: "https://go.koacher.app/",
   },
   {
     id: 2,
-    title: "Application Web Portfolio",
+    title: "Landing Page pour coachs",
     description:
-      "Portfolio interactif moderne avec React.js et TailwindCSS pour présenter des projets et compétences.",
+      "Page de destination dynamique et optimisée pour les coachs, mettant en avant leurs services avec des animations modernes et une compatibilité mobile.",
+    image: "https://www.koacher.app/phoneheader.png",
+    tags: ["React.js", "Next.js", "TailwindCSS", "TypeScript", "MongoDB"],
+    link: "https://www.koacher.app/",
+  },
+  {
+    id: 3,
+    title: "Landing Page pour sportifs",
+    description:
+      "Page de destination dédiée aux sportifs, présentant des fonctionnalités d'inscription et des outils interactifs pour leur progression.",
+    image: "https://www.koacher.app/phoneheader.png",
+    tags: ["React.js", "Next.js", "TailwindCSS", "TypeScript", "MongoDB"],
+    link: "https://pro.koacher.app/",
+  },
+  {
+    id: 4,
+    title: "Portfolio interactif",
+    description:
+      "Un portfolio interactif et moderne utilisant des animations avancées avec Framer Motion pour mettre en valeur vos compétences et projets.",
     image: "https://assets.genially.com/s3fs-public/Portfolio%20designfr.png",
     tags: ["React.js", "TypeScript", "TailwindCSS", "Framer Motion"],
     link: "#",
   },
   {
-    id: 3,
-    title: "API Supervision et Déploiement de Systèmes Informatiques",
+    id: 5,
+    title: "API de supervision et déploiement",
     description:
-      "Backend RESTful modulaire utilisant Node.js, Express.js et Docker pour une architecture microservices robuste.",
+      "API RESTful modulaire permettant la supervision et le déploiement de systèmes informatiques avec une architecture robuste et des outils CI/CD.",
     image:
       "https://miro.medium.com/v2/resize:fit:1400/1*UumOs0Gk9x4P_sNwqHvYjA.png",
-    tags: [
-      "Node.js",
-      "Jenkins",
-      "Devops",
-      "Docker",
-      "CI/CD Pipelines",
-      "AWS",
-      "Grafana",
-    ],
+    tags: ["Node.js", "Jenkins", "Docker", "CI/CD Pipelines", "AWS", "Grafana"],
     link: "https://api.koacher.app/",
   },
   {
-    id: 4,
-    title: "Logiciel de sauvegarde",
+    id: 6,
+    title: "Logiciel de sauvegarde automatique",
     description:
-      "Logiciel desktop pour sauvegarde automatique des fichiers, construit avec ASP.Net et WPF.",
+      "Une solution desktop pour la sauvegarde automatique de fichiers avec une interface intuitive développée sous ASP.Net et WPF.",
     image: "https://grandgirardclement.eu/site/assets/img/easySave.png",
     tags: ["ASP.Net", "C#", "MVC", "WPF", "SQL Server"],
     link: "https://github.com/VictorMassotte/EasySave",
   },
   {
-    id: 5,
+    id: 7,
     title: "Admin Panel de supervision",
     description:
-      "Interface d'administration Next.js avec backend Nest.js pour la gestion des utilisateurs et permissions.",
+      "Un tableau de bord d'administration utilisant Next.js et Nest.js, conçu pour la gestion des utilisateurs, permissions et statistiques.",
     image: "https://www.capensis.fr/wp-content/uploads/listalarm.png",
-    tags: ["Next.js", "Nest.js", "TypeScript", "Node.js", "AWS", "graphQL"],
+    tags: ["Next.js", "Nest.js", "TypeScript", "Node.js", "GraphQL", "AWS"],
     link: "https://admin.koacher.app",
   },
   {
-    id: 6,
+    id: 8,
     title: "Application mobile de pointage IA",
     description:
-      "Application mobile avec IA pour la reconnaissance faciale et le suivi de la présence des employés.",
+      "Application mobile intégrant la reconnaissance faciale basée sur l'IA pour gérer efficacement la présence des employés.",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4EtCn-VeNU5viQ_Wsd1Ricu4IXvZG1ss1BA&s",
     tags: ["React.js", "TypeScript", "Python", "IA", "Firebase"],
     link: "https://expo.dev/accounts/anisdz99/projects",
   },
   {
-    id: 7,
+    id: 9,
     title: "Application mobile de recyclage",
     description:
-      "Application intuitive offrant des conseils de recyclage avec une interface utilisateur réactive.",
+      "Application éducative offrant des conseils pratiques sur le recyclage avec une interface moderne et intuitive.",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7EZBWXVIuwLA7qmTKDe0_gOKWw0mrrDUMOw&s",
     tags: ["React Native", "Expo", "TailwindCSS", "PHP", "AWS"],
     link: "https://expo.dev/accounts/anisdz99/projects",
   },
   {
-    id: 8,
-    title: "Application mobile de sport",
+    id: 10,
+    title: "Application mobile pour le sport",
     description:
-      "Système complet de gestion de contenu (CMS) développé avec Symfony et PHP.",
+      "Application mobile conçue pour les sportifs, offrant un système complet de gestion de contenu (CMS) avec Symfony et React Native.",
     image: "https://www.koacher.app/phoneheader.png",
     tags: ["Symfony", "PHP", "MySQL", "Expo", "Doctrine", "React Native"],
     link: "https://apps.apple.com/fr/app/koacher/id6479816738",
